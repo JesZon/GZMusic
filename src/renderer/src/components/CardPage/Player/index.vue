001128ce78751706c80c04f8d24cbe5e6e425e27
JesZon <jeszhisheng@outlook.com>
2025-04-27 22:10:04 +0800

    优化

--- src/renderer/src/components/CardPage/Player/index.vue
+++ src/renderer/src/components/CardPage/Player/index.vue
@@ -6,10 +6,7 @@
                 {{ RetrievingReleasing ? '收起' : '展开' }}
             </span>
         </div>
-        <div style="transition: all 0.8s ease;z-index: 5;"
-            :style="RetrievingReleasing ? 'transform: scale(1);' : 'transform: scale(0.8);'">
-            <img src="./astronaut.png" alt="" class="image" />
-        </div>
+        <img src="./astronaut.png" alt="" class="image" />
         <div class="heading">
             <div v-if="RetrievingReleasing">
                 知我 - 未知歌手
@@ -102,21 +99,20 @@
                             <path d="M544 672V352h-48L416 409.6l16 41.6 60.8-41.6V672z" p-id="2038" fill="#f9f9fb">
                             </path>
                         </svg>
-                        <svg v-if="getPlayType === 'random'" t="1745137982545" class="prev-type-mini" viewBox="0 0 1024 1024"
-                            version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2199">
+                        <svg v-if="getPlayType === 'random'" t="1745137982545" class="prev-type-mini"
+                            viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2199">
                             <path
                                 d="M844.8 665.6c-6.4-6.4-16-12.8-25.6-9.6-19.2 0-35.2 16-35.2 35.2 0 9.6 6.4 19.2 12.8 25.6l41.6 41.6c-44.8-6.4-86.4-22.4-121.6-51.2-3.2 0-3.2-3.2-6.4-6.4L332.8 304C268.8 233.6 192 195.2 99.2 195.2c-19.2 0-35.2 16-35.2 35.2s16 32 35.2 32c73.6 0 134.4 32 182.4 86.4l384 400 6.4 6.4c48 38.4 108.8 64 172.8 70.4l-48 44.8c-9.6 6.4-16 19.2-16 28.8 0 19.2 19.2 35.2 38.4 32 9.6 0 19.2-6.4 25.6-12.8l99.2-92.8c16-16 16-41.6 0-57.6l-99.2-102.4z m-3.2-556.8c-12.8-16-32-19.2-48-6.4-9.6 6.4-12.8 16-12.8 25.6 0 12.8 3.2 22.4 16 28.8l41.6 41.6c-73.6 9.6-140.8 38.4-192 89.6l-115.2 118.4c-12.8 12.8-12.8 32 0 44.8 6.4 6.4 16 9.6 25.6 9.6s19.2-3.2 25.6-9.6l112-118.4c41.6-38.4 92.8-64 147.2-70.4l-44.8 44.8c-6.4 6.4-12.8 16-12.8 25.6 0 19.2 16 35.2 32 35.2 9.6 0 19.2-3.2 28.8-9.6L950.4 256c12.8-12.8 12.8-35.2 0-48l-108.8-99.2m-438.4 448c-9.6 0-19.2 3.2-25.6 9.6l-118.4 121.6c-48 44.8-96 67.2-160 67.2H96c-19.2 0-35.2 16-35.2 35.2s16 32 35.2 32h3.2c83.2 0 147.2-32 211.2-86.4l121.6-124.8c6.4-6.4 9.6-12.8 9.6-22.4 0-9.6-3.2-16-9.6-22.4-9.6-6.4-19.2-9.6-28.8-9.6z"
                                 p-id="2200" fill="#f9f9fb"></path>
                         </svg>
                     </div>
                     <div class="volume-control">
-                        <icon-sound-fill class="prev-type-mini"/>
+                        <icon-sound-fill class="prev-type-mini" />
                     </div>
                 </a-space>
             </a-space>
 
         </template>
-
     </div>
 </template>
 
@@ -138,15 +134,15 @@ const playList = ref(['cycle', 'single', 'random']);
 const playType = ref(0);
 
 const handleEscKey = (event) => {
-  if (event.key === ' ') {
-    isPlaying.value = !isPlaying.value;
-  }
+    if (event.key === ' ') {
+        isPlaying.value = !isPlaying.value;
+    }
 };
 onMounted(() => {
-  window.addEventListener('keydown', handleEscKey);
+    window.addEventListener('keydown', handleEscKey);
 });
 onUnmounted(() => {
-  window.removeEventListener('keydown', handleEscKey);
+    window.removeEventListener('keydown', handleEscKey);
 });
 
 
--- src/renderer/src/components/CardPage/index.vue
+++ src/renderer/src/components/CardPage/index.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="Content">
-        <div class="left" :style="{ width: RetrievingReleasing ? '40%' : '10%' }">
-            <Player />
-        </div>
-        <div class="right" :style="{ width: RetrievingReleasing ? '60%' : '90%' }">
-            <ContentBar />
-        </div>
+  <div class="Content">
+    <div class="left" :style="{ width: RetrievingReleasing ? '40%' : '10%' }">
+      <Player />
     </div>
+    <div class="right" :style="{ width: RetrievingReleasing ? '60%' : '90%' }">
+      <ContentBar />
+    </div>
+    <audio controls>
+      <source src="https://m701.music.126.net/20250425013206/0c7b27e2e2db61d101a0b8f01105f801/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/45265663668/670c/1f97/8b21/e4cfea741bdb9c0075349fa189b03439.mp3?vuutv=QlYk2TQ6zMEhdj4MWesi+CCIAkkX+6/viadJ8GM7xt+SPOtW+Zn8kHm0f34cLQB7gc5UQZ0Umd8NaW+Vv3blXBPF/ShlgzVtD7JQLGMOPQkuqVr9rduKL0INlHgGRO/kVQ0IXIMZ6hnFCoOZNtrVzwz8w3oiXhb6F1GToZ3ig5EVIenOiUshZrYeeB687YvKlhxlUf9T87VpD6Y1d92gNe85c4MOLGBzONsMaI7D1eeGVC6vgb95yCIpjoXPpODeyHJ+AO+wvyuH2J0LkdWp0TlzFFSwQL3PszGUko1PfsT9RCD4fLVPql/q1rfIATAhiMWwUhmTVxAbaDu6GP4vGJTwH/xNTrkEPPWguaW3pLscckO55CJOnUbbOSuVeCy/" type="audio/mpeg">
+      你的浏览器不支持音频播放。
+    </audio>
+  </div>
 </template>
 
 <script lang="ts" setup name="CardPage">
@@ -35,27 +39,31 @@ onUnmounted(() => {
 </script>
 
 <style lang="less" scoped>
-.Content{
-    width: 100vw;
-    height: 80vh;
+.Content {
+  width: 100vw;
+  height: 80vh;
 }
 
-.left{
-    float: left;
-    height: 100%;
-    transition: width 0.5s ease;
+.left {
+  float: left;
+  height: 100%;
+  transition: width 0.5s ease;
 
-    display: flex;
-    align-items: center; /* 垂直居中 */
-    justify-content: center; /* 水平居中 */
+  display: flex;
+  align-items: center;
+  /* 垂直居中 */
+  justify-content: center;
+  /* 水平居中 */
 }
 
-.right{
-    float: right;
-    height: 100%;
-    transition: width 0.5s ease;
-    display: flex;
-    align-items: center; /* 垂直居中 */
-    justify-content: center; /* 水平居中 */
+.right {
+  float: right;
+  height: 100%;
+  transition: width 0.5s ease;
+  display: flex;
+  align-items: center;
+  /* 垂直居中 */
+  justify-content: center;
+  /* 水平居中 */
 }
 </style>
